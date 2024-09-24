44e23502c931f29d262bf0509cb8c9802c4ee717
evelynkim321 <hkim321@my.centennialcollege.ca>
2024-09-23 22:43:22 -0400

    update1

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,18 +8,22 @@ const Projects = () => {
       <div className="project-list">
         <div className="project">
           <img src="project1-image.jpg" alt="Project 1" />
-          <h2>Project 1</h2>
-          <p>Short description of Project 1.</p>
+          <h2>Project 1: Web Design - Commercial Product Website</h2>
+          <p>*Designed an interactive marketing website using JavaScript, HTML, and CSS<br></br>
+          *Developed an attractive, responsive and user-oriented interface for ease of navigation </p>
         </div>
         <div className="project">
           <img src="project2-image.jpg" alt="Project 2" />
-          <h2>Project 2</h2>
-          <p>Short description of Project 2.</p>
+          <h2>Project 2:Commercial Web Application Software Requirements Specification </h2>
+          <p>*Developed a detailed software requirement specification by using both textual and graphical diagrams (UML
+            Standards) for a fictitious web application,<br></br> gaining a solid understanding of technical documentation.</p>
         </div>
         <div className="project">
           <img src="project3-image.jpg" alt="Project 3" />
-          <h2>Project 3</h2>
-          <p>Short description of Project 3.</p>
+          <h2>Project 3: E-commerce Database Design – Backend Project</h2>
+          <p>*Built an e-commerce database with SQL Developer, including ERDs and a populated schema<br></br>
+            *Executed SQL queries (DDL, DML) to manage data and implement features like product categorization and
+            shopping cart operations</p>
         </div>
       </div>
     </div>
